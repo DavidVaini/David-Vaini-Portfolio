5328fb09742d228d0de6d71f289d676519a1af3c
davidvaini <davidvaini@gmail.com>
2014-12-28 14:26:34 -0600

    getting a start on the wireframe/layout of the page.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+
+
+$(document).foundation({
+  offcanvas : {
+    // Sets method in which offcanvas opens.
+    // [ move | overlap_single | overlap ]
+    open_method: 'move', 
+    // Should the menu close when a menu link is clicked?
+    // [ true | false ]
+    close_on_click : false
+  }
+});
+
+$(document).on('open.fndtn.offcanvas', '[data-offcanvas]', function () {
+  var off_canvas_wrap = $(this);
+});
+
+$(document).on('close.fndtn.offcanvas', '[data-offcanvas]', function () {
+  var off_canvas_wrap = $(this);
+});
